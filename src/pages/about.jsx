b4bcc082c0ac6f8acc3bed7a3a3db71b03cf637d
arata sekine <arata-sekine@aeonlink.co.jp>
2018-06-15 18:24:17 +0900

    yarn install

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default class App extends React.Component {
-  render() {
-    const message = 'About webpack.';
-    return (
-      <div>
-        <Header/>
-        <p>{message}</p>
-      </div>
-    );
-  }
+    render() {
+        const message = 'About webpack.';
+        return (
+            <div>
+                <Header/>
+                <p>{message}</p>
+            </div>
+        );
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,45 @@
 const path = require('path');
+// webpackのライブラリを読み込む
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+  //webpackを実行するファイルを指定する
   entry: {
     index: './src/index.js',
     about: './src/about.js',
     vendor: ['react', 'react-dom']
   },
+  //ビルド後のファイル名とパスを指定
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].bundle.js',// [name]はentryで設定したnameに置き換わる
     path: path.resolve(__dirname, 'dist')
   },
+  //ビルド対象の拡張子設定
   resolve: {
-    extensions: ['.js', '.json', '.jsx']
+    extensions: ['.js', '.json', '.jsx','.txt']
   },
   module: {
+      //Loaderの対象の拡張子をtest:に設定
+      //使用するLoaderをuser:に設定
+      //特定のディレクトリ配下をLoaderの適用対象外としたい場合などにexclude:に設定
     rules: [
+        //raw-loaderはimportしたファイルを文字列として処理してくれるLoader
       {
         test: /\.txt$/,
         use: 'raw-loader'
       },
       {
+        //BabelのLoader設定
         test: /(\.js$|\.jsx$)/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: 'babel-loader'
       },
       {
+        //CSS用のLoader設定
+        //sass-loader	CSSのトランスパイル
+        //css-loader  CSSの@importとurl()のパス解決
+        //Loaderのuse指定にExtractTextPlugin.extractを組み合わせる
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           use: [{
@@ -45,12 +56,16 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
-      minChunks: Infinity
-    }),
-    new ExtractTextPlugin({
-      filename: '[name].css'
-    })
+      //CommonsChunkPlugin：重複したコードを抜き出すプラグイン
+      //重複して使用されているチャンクを共通チャンクとして出力
+      new webpack.optimize.CommonsChunkPlugin({
+          //module.exports.name
+          name: "vendor", // entryに追加したチャンク名
+          minChunks: Infinity // 他のモジュールをこのチャンクに含めないことを保証
+      }),
+     　// ExtractTextWebpackPlugin：特定のテキストを抜き出す
+      new ExtractTextPlugin({
+        filename: '[name].css'
+      })
   ]
 };
